adff4be0591fad487f31b116798226eade7d0de8
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2024-12-24 14:58:25 +0700

    24.12.2024 14:58

---
+++ scripts/featured-offer-section.js
@@ -0,0 +1,5 @@
+const featuredOfferBanner = document.querySelector('.featured-offer-banner');
+
+featuredOfferBanner.addEventListener('click', () => {
+    window.open('https://www.irobot.com/en_US/roomba-combo-10-max-robot-with-autowash-dock/X085020.html', '_blank');
+});
