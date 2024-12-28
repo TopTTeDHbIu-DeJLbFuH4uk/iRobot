306beeedd13c14fe455503c91ba6c1d160990a46
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2024-12-29 01:05:04 +0700

    29.12.2024 01:05

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,108 +1,52 @@
-// const navItemEls = [...document.querySelectorAll('.nav-item')];
-// const containerPopupMenuEls = [...document.querySelectorAll('.container-popup-menu')];
-//
-// window.addEventListener('click', e => showMenu(e));
-//
-// const showMenu = e => {
-//
-//     console.log(e.composedPath());
-//
-//     const containerDropdownMenuShop = e.target.closest('.container-popup-menu');
-//     const navItem = e.target.closest('.nav-item');
-//
-//     if (!navItem && !containerDropdownMenuShop) {
-//         containerPopupMenuEls.forEach(menu => menu.classList.add('container-popup-menu-hidden'));
-//         navItemEls.forEach(item => item.classList.remove('select-nav-item'));
-//         return;
-//     }
-//
-//     if (navItem) {
-//         const dataNavItemId = navItem.dataset.navItem;
-//
-//         navItemEls.forEach(item => {
-//             if (item === navItem) {
-//                 item.classList.toggle('select-nav-item');
-//             } else {
-//                 item.classList.remove('select-nav-item');
-//             }
-//         });
-//
-//         containerPopupMenuEls.forEach(menu => {
-//             if (menu.getAttribute('data-dropdown-menu') === dataNavItemId) {
-//                 menu.classList.toggle('container-popup-menu-hidden');
-//             } else {
-//                 menu.classList.add('container-popup-menu-hidden');
-//             }
-//         });
-//     }
-// };
+const popupEls = [...document.querySelectorAll('.popup')];
+const navItemEls = [...document.querySelectorAll('.nav_item')];
+let lastSelectedPopupIndex;
 
-const navItemEls = [...document.querySelectorAll('.nav-item')];
-const containerPopupMenuEls = [...document.querySelectorAll('.container-popup-menu')];
+navItemEls.forEach((itemEl, index) => {
+    itemEl.addEventListener('click', () => {
 
-navItemEls.forEach(item => {
-    item.addEventListener('click', e => {
-
-        const itemId = item.dataset.navItem;
-
-        containerPopupMenuEls.forEach(popup => {
-
-            if (itemId === popup.getAttribute('data-dropdown-menu')) {
-                popup.classList.remove('container-popup-menu-hidden');
-
-                window.addEventListener('click', e => closePopup(e));
-
-            } else if (itemId !== popup.getAttribute('data-dropdown-menu')) {
-                popup.classList.add('container-popup-menu-hidden');
-            }
-
-        });
-
-        e.stopPropagation();
-    });
-});
-
-
-const closePopup = e => {
-
-    const path = e.composedPath();
-
-    containerPopupMenuEls.forEach(popup => {
-
-        if (!path.includes(popup)) {
-            popup.classList.add('container-popup-menu-hidden');
+        if (lastSelectedPopupIndex === index) {
+            popupEls[lastSelectedPopupIndex].classList.remove('popup_active');
+            lastSelectedPopupIndex = undefined;
+            return;
         }
-    });
 
-    window.removeEventListener('click', closePopup);
-};
+        if (lastSelectedPopupIndex !== undefined) {
+            popupEls[lastSelectedPopupIndex].classList.remove('popup_active');
+        }
 
-
-
-
-
-
-
-
-
-
-const menuCategoryEls = [...document.querySelectorAll('.menu-link')];
-const robotCategoryShopEls = [...document.querySelectorAll('.robot-image')];
-
-menuCategoryEls.forEach(menu => {
-    menu.addEventListener('mousemove', () => {
-
-        const dataMenuId = menu.dataset.menu;
-
-        robotCategoryShopEls.forEach(robot => {
-
-            if (dataMenuId === robot.getAttribute('data-robot')) {
-                robot.classList.remove('robot-hidden');
-
-            } else if (dataMenuId !== robot.getAttribute('data-robot')) {
-                robot.classList.add('robot-hidden');
-            }
-        });
+        lastSelectedPopupIndex = index;
+        popupEls[index].classList.add('popup_active');
     });
 });
 
+window.addEventListener('click', e => {
+
+    const popup = e.target.closest('.popup');
+    const navItem = e.target.closest('.nav_item');
+
+    if (!popup && !navItem)  {
+        popupEls[lastSelectedPopupIndex].classList.remove('popup_active');
+    }
+});
+
+
+
+
+const menuItemEls = [...document.querySelectorAll('.menu_item')];
+const robotEls = [...document.querySelectorAll('.robot')];
+const robotActive = 'robot_active';
+let lastSelectedRobotIndex;
+
+menuItemEls.forEach((menuEl, index) => {
+    menuEl.addEventListener('mouseenter', () => {
+        if (lastSelectedRobotIndex === index) return;
+
+        if (lastSelectedRobotIndex !== undefined) {
+            robotEls[lastSelectedRobotIndex].classList.remove(robotActive);
+        }
+
+        lastSelectedRobotIndex = index;
+        robotEls[index].classList.add(robotActive);
+    });
+});
