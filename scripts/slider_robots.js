1954f359e97df762f4ce92cd17a8ca42e98c7916
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2025-01-05 21:07:07 +0700

    05.01.2025 21:07

--- scripts/slider_robots.js
+++ scripts/slider_robots.js
@@ -1,11 +1,29 @@
+const paginationCirclesContainerEl = document.querySelector('.pagination_circles_container');
 const btnPrevPositionEl = document.querySelector('.btn_prev_position');
 const btnNextPositionEl = document.querySelector('.btn_next_position');
 const sliderRobotsTrackEl = document.querySelector('.slider_robots_track');
 const sliderRobotWrapperEls = [...document.querySelectorAll('.slider_robot_wrapper')];
 
-let currentSlideIndex = 0;
+// Переменные для работы со слайдами
 const visibleSlidesCount = 3;
+let currentSlideIndex = visibleSlidesCount;
 let isAnimating = false;
+
+// Создание кружков пагинации в количестве слайдов
+const paginationCircles = [];
+
+const createPaginationCircles = () => {
+    const li = document.createElement('li');
+    li.className = 'pagination_circle';
+    paginationCirclesContainerEl.appendChild(li);
+    paginationCircles.push(li);
+};
+
+const addPagination = () => {
+    sliderRobotWrapperEls.forEach(createPaginationCircles);
+    paginationCircles[0].classList.add('circle_active');
+};
+addPagination();
 
 // Базовая ширина слайда
 let slideWidth = sliderRobotWrapperEls[0].offsetWidth;
@@ -26,7 +44,6 @@ firstThreeSlides.forEach(slideEl => {
 
 // Новый список слайдов (12)
 const allSlides = [...document.querySelectorAll('.slider_robot_wrapper')];
-currentSlideIndex = visibleSlidesCount;
 
 // Динамическое обновление ширины слайда при масштабировании
 const updateSlideWidth = () => {
@@ -36,24 +53,67 @@ const updateSlideWidth = () => {
 window.addEventListener('resize', updateSlideWidth);
 
 // Двигать трэк со слайдами
-const updateSliderPosition = (withTransition = true) => {
+const updateSliderPosition = (withTransition = true, index) => {
+
+    if (index) {
+        removeActiveClass(currentSlideIndex);
+        currentSlideIndex = index;
+        addActiveClass(currentSlideIndex);
+    }
+
     const offset = -currentSlideIndex * slideWidth;
 
     sliderRobotsTrackEl.style.transition = withTransition ? 'transform .5s' : 'none';
     sliderRobotsTrackEl.style.transform = `translate3d(${offset}px, 0px, 0px)`;
 };
 
+// Навигация по кружкам пагинации
+paginationCircles.forEach((circleEl, index) => {
+    circleEl.addEventListener('click', () => {
+        updateSliderPosition(true, index + visibleSlidesCount);
+    });
+});
+
+// Добавлять выделенный кружок пагинации текущему слайду
+const addActiveClass = (currentSlideIndexCircle) => {
+    let normalizedIndex;
+
+    if (currentSlideIndexCircle) {
+        normalizedIndex = (currentSlideIndexCircle - visibleSlidesCount + paginationCircles.length) % paginationCircles.length;
+    } else {
+        normalizedIndex = (currentSlideIndex - visibleSlidesCount + paginationCircles.length) % paginationCircles.length;
+    }
+
+    paginationCircles[normalizedIndex].classList.add('circle_active');
+};
+
+// Удалять выделенный кружок пагинации предыдущему слайду
+const removeActiveClass = (currentSlideIndexCircle) => {
+    let normalizedIndex;
+
+    if (currentSlideIndexCircle) {
+        normalizedIndex = (currentSlideIndexCircle - visibleSlidesCount + paginationCircles.length) % paginationCircles.length;
+    } else {
+        normalizedIndex = (currentSlideIndex - visibleSlidesCount + paginationCircles.length) % paginationCircles.length;
+    }
+    paginationCircles[normalizedIndex].classList.remove('circle_active');
+};
+
 // Показать следующий слайд
 const nextSlide = () => {
-    // Блокировка спама кликами
+    // Блокировать клик до завершения анимации
     if (isAnimating) return;
     isAnimating = true;
     setTimeout(() => {
         isAnimating = false;
     }, 500);
 
+    removeActiveClass();
+
     currentSlideIndex++;
     updateSliderPosition();
+
+    addActiveClass();
 
     // Быстрая перемотка назад при достижении последнего клона
     if (currentSlideIndex === allSlides.length - visibleSlidesCount) {
@@ -67,15 +127,19 @@ const nextSlide = () => {
 
 // Показать предыдущий слайд
 const prevSlide = () => {
-    // Блокировка спама кликами
+    // Блокировать клик до завершения анимации
     if (isAnimating) return;
     isAnimating = true;
     setTimeout(() => {
         isAnimating = false;
     }, 500);
 
+    removeActiveClass();
+
     currentSlideIndex--;
     updateSliderPosition();
+
+    addActiveClass();
 
     // Быстрая перемотка назад при достижении последнего клона
     if (currentSlideIndex === 0) {
@@ -87,40 +151,9 @@ const prevSlide = () => {
     }
 };
 
-const posInit = 0;
-let posX1 = 0;
-let posX2 = 0;
-
-const swipeStart = () => {
-
-};
-
-const swipeAction = () => {
-
-};
-
-const swipeEnd = () => {
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Обработчики на кнопках
 btnNextPositionEl.addEventListener('click', nextSlide);
 btnPrevPositionEl.addEventListener('click', prevSlide);
 
 // Инициализация начального положения слайдера
 updateSliderPosition(false);
-
-
