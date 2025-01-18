8500e5afafde8aeb1749b66de85c625f184cfd54
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2025-01-18 23:01:48 +0700

    18.01.2025 23:02

--- mobile_scripts/mobile_navigation.js
+++ mobile_scripts/mobile_navigation.js
@@ -1,40 +1,35 @@
 const burgerMenuEl = document.querySelector('.burger_menu');
 const closeMenuEl = document.querySelector('.close_menu');
-const mobileMenuContainerEl = document.querySelector('.mobile_menu_container');
+const dropDownMenuEl = document.querySelector('.drop_down_menu');
 
 burgerMenuEl.addEventListener('click', () => {
-    mobileMenuContainerEl.style.transform = 'translateX(0)';
+    dropDownMenuEl.style.transform = 'translateX(0)';
+    document.body.classList.add('scroll_hidden');
 });
 
 closeMenuEl.addEventListener('click', () => {
-    console.log('Закрытие меню');
-    // mobileMenuContainerEl.style.backgroundColor = 'red'; // Работает
-    mobileMenuContainerEl.style.transform = 'translateX(-100%)';
+    dropDownMenuEl.style.transform = 'translateX(-100%)';
+    document.body.classList.remove('scroll_hidden');
 });
 
+const mobileNavItemEls = [...document.querySelectorAll('.mobile_nav_item')];
+const menuItemContainerEl = document.querySelector('.menu_item_container');
+const btnBackEl = document.querySelector('.btn_back');
 
+btnBackEl.addEventListener('click', () => {
+    menuItemContainerEl.style.transform = `translateX(490px)`;
+    btnBackEl.classList.remove('visibility');
+});
 
+const openMobileMenu = itemMenu => {
 
+    itemMenu.addEventListener('click', () => {
+        btnBackEl.classList.add('visibility');
+        menuItemContainerEl.style.transform = 'translateX(0)';
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
+mobileNavItemEls.forEach(openMobileMenu);
 
 
 const mobileSelectedLanguageEl = document.querySelector('.mobile_selected_language');
